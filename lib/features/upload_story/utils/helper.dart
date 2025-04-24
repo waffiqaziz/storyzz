a66cdcca99e71c09c2046b95c8b1e2a865f378da
WaffiqA <waffiq.a2002@gmail.com>
2025-04-24 20:11:37 +0700

    fix: resolve unused

--- lib/features/upload_story/utils/helper.dart
+++ lib/features/upload_story/utils/helper.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:storyzz/core/localization/l10n/app_localizations.dart';
-import 'package:storyzz/features/upload_story/data/model/country_coordinates.dart';
 
 /// Used as helper to transform error message from location operation
 /// into error message based on localization (english or indonesian)
@@ -18,7 +17,7 @@ String getLocalizedErrorMessage(BuildContext context, String error) {
   return AppLocalizations.of(context)!.location_error;
 }
 
-LatLng? getCountryCoordinates(String countryCode) {
-  // Convert to uppercase for case-insensitive lookup
-  return countryCoordinates[countryCode.toUpperCase()];
-}
+// LatLng? getCountryCoordinates(String countryCode) {
+//   // Convert to uppercase for case-insensitive lookup
+//   return countryCoordinates[countryCode.toUpperCase()];
+// }
