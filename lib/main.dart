a6f3455e469650e3cb893ce7c9bf4b65ba7dafc1
Waffiq Aziz <59906896+waffiqaziz@users.noreply.github.com>
2026-04-28 09:06:19 +0700

    chore(web): remove hash fragment from URLs (#211)

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:storyzz/app_root.dart';
 import 'package:storyzz/core/utils/environment.dart';
 import 'package:storyzz/core/variant/build_configuration.dart';
+import 'package:storyzz/common/url_strategy.dart';
 
 void main() async {
   // ensure that plugin services are initialized before using any plugins.
   // this is especially important for plugins that require platform channels.
   WidgetsFlutterBinding.ensureInitialized();
+
+  usePathUrlStrategy();
 
   // show url changes
   GoRouter.optionURLReflectsImperativeAPIs = true;
