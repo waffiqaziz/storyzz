956a70ffcbb37a854aa7b23a41f5b3d3bf6fba74
WaffiqA <waffiq.a2002@gmail.com>
2026-07-06 15:54:58 +0700

    chore: fix breaking changes on geocoding 5.0.0
    https://pub.dev/packages/geocoding/changelog#500

--- lib/features/detail/presentation/providers/geocoding_provider.dart
+++ lib/features/detail/presentation/providers/geocoding_provider.dart
@@ -8,6 +8,7 @@ class GeocodingProvider extends ChangeNotifier {
 
   // injection for testing
   final Future<List<Placemark>> Function(double, double)? geocodingFunction;
+  final Geocoding geocoding = Geocoding();
 
   GeocodingProvider({this.geocodingFunction});
 
@@ -21,7 +22,7 @@ class GeocodingProvider extends ChangeNotifier {
     try {
       final placemarks = geocodingFunction != null
           ? await geocodingFunction!(latitude, longitude)
-          : await placemarkFromCoordinates(latitude, longitude);
+          : await geocoding.placemarkFromCoordinates(latitude, longitude);
 
       if (placemarks.isEmpty) {
         _state = const GeocodingState.error('No address found');
